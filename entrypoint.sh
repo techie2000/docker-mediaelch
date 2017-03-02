7e3c4d0456a212bfe75d703493bdcec40cb89772
odon <romain.veillerot@gmail.com>
2017-03-02 09:28:05 +0100

    edd entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+PUID=${PUID:20000}
+PGID=${PGID:20000}
+
+groupmod -o -g "$PGID" mediaelch
+usermod -o -u "$PUID" mediaelch
+
+chown -R mediaelch:mediaelch /home/mediaelch
+
+exec "$@"
